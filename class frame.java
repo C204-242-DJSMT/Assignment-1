67b1603eef09d3767482569966a2736df6085616
spacewolf009 <spacewolf009@gmail.com>
2012-08-07 14:38:56 +1200

    Stuff from meeting 3. Start laying out your classes etc. meeting friday at 12 (unless otherwise arranged)

---
+++ class frame.java
@@ -0,0 +1,56 @@
+class Package {
+	string id
+	Client sender
+	reciever location etc can be account holder or not
+	Scan for timing null for not scanned
+	parent bin
+
+}
+
+class Scan {
+	some kind of identifier
+	timestamp
+}
+
+class Bin {
+	static collection
+	string id 
+	collection<city> destinations
+	collection of all packages in the bin
+	scan methods for the bin aplies scan to everything inside
+	vehicle identifier
+
+}
+class City {
+	name
+
+}
+class Destination {
+	Client - null if non client
+	string Address
+	string postcode
+}
+
+class Client {
+	static collection
+	string id
+	ArrayList<destination> addresses
+	access to packages addressed to them
+}
+
+class Employee {
+	string id
+	(enum) role
+}
+
+class Driver extends Employee{
+	string vehicle id
+	some way to get manifest
+}
+
+static class DataAdapter {
+	static ArrayLists of Packages, clients, cities, bins, employees
+	static methods to access them
+}
+
+enum employee roles
