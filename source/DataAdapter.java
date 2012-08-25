5a9fcfa3c700b8eac739053eedece69ccc353ed5
spacewolf009 <spacewolf009@gmail.com>
2012-08-26 10:05:25 +1200

    Added skeleton classes for all classes disscussed so far

---
+++ source/DataAdapter.java
@@ -0,0 +1,99 @@
+import java.util.ArrayList;
+
+/*
+ * 
+ */
+class DataAdapter {
+	private static ArrayList<Package> allPackages = new ArrayList<Package>();
+
+	/*
+ 	* 
+ 	*/
+	public Package getPackageByID() {
+		return null;
+	}
+
+	/*
+ 	* 
+ 	*/
+	public ArrayList<Package> getPackages() {
+		return allPackages;
+	}
+
+	/*
+	 * Adds a package to the list if it is a valid object and not conflicting with the current list contets.
+	 * Return true if it was successfully added to the list.
+	 */
+	public boolean addPackage(Package p) {
+		return false;
+	}
+
+	private static ArrayList<Client> allClients = new ArrayList<Client>();
+
+	/*
+ 	* 
+ 	*/
+	public Client getClientByID() {
+		return null;
+	}
+
+	/*
+ 	* 
+ 	*/
+	public ArrayList<Client> getClients() {
+		return allClients;
+	}
+
+	/*
+	 * 
+	 */
+	public boolean addClient(Client c) {
+		return false;
+	}
+
+	private static ArrayList<Employee> allEmployees = new ArrayList<Employee>();
+
+	/*
+ 	* 
+ 	*/
+	public Employee getEmployeeByID() {
+		return null;
+	}
+
+	/*
+ 	* 
+ 	*/
+	public ArrayList<Employee> getEmployees() {
+		return allEmployees;
+	}
+
+	/*
+	 * 
+	 */
+	public boolean addEmployee(Employee e) {
+		return false;
+	}
+
+	private static ArrayList<PackageBin> allPackageBins = new ArrayList<PackageBin>();
+	/*
+ 	* 
+ 	*/
+	public PackageBin getBinByID() {
+		return null;
+	}
+
+	/*
+ 	* 
+ 	*/
+	public ArrayList<PackageBin> getBins() {
+		return allPackageBins;
+	}
+
+	/*
+	 * 
+	 */
+	public boolean addBin(PackageBin c) {
+		return false;
+	}
+
+}
