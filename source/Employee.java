3622c037318e9d15f3ca9cd8bfff60446bf526a8
spacewolf009 <spacewolf009@gmail.com>
2012-08-31 10:48:17 +1200

    Update source/Employee.java
    added username member to Employee

--- source/Employee.java
+++ source/Employee.java
@@ -11,12 +11,16 @@ class Employee {
 
 	final long id;
 	EmployeeRoles role;
+	final String username;
 
 	/*
 	 * 
 	 */
-	public Employee(EmployeeRoles role) {
+	public Employee(EmployeeRoles role, String username) {
+		if (username == null || username.length == 0) //must also check username is not already in use
+			throw new IllegalArgumentException();
 		this.role = role;
+		this.username = username;
 		this.id = NextID;
 		NextID++;
 		DataAdapter.addEmployee(this);
