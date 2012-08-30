b073728fe736f2bd1377b67f72f36ed4b165409b
Mathew Andela <matcour@windowslive.com>
2012-08-30 22:35:10 +1200

    Drivers stuff manifest(deliver/pickup)

---
+++ Drivers/Package.java
@@ -0,0 +1,48 @@
+import java.util.EnumMap;
+ 
+/*
+ * Enumeration for each occassion a package may or must be scanned.
+ */
+enum ScanTypes {placeholder};
+//Package and PackageBin may be able to support a common superclass or interface
+class Package {
+	static private long NextID = 0;
+	final long id;
+
+	Client sender;
+	Client addressee; // Can be null if not a client
+	String streetAddress;
+	String postcode;
+	String destinationCity;
+	String senderAddress;
+	String senderPostcode;
+	String status;
+	EnumMap<ScanTypes, Scan> scanHistory = new EnumMap<ScanTypes, Scan>(ScanTypes.class);
+/*
+	void scan(ScanTypes scanType, Employee scanner) {
+		if (this.scanHistory.get(scanType == null))
+			this.scanHistory.put(scanType, new Scan(scanner));
+		else
+			throw new IllegalArgumentException("Package already contains a record for that scan");
+	}
+
+	/*
+	 * 
+	 */
+	public Package(Client sender, Client addressee, String streetAddress, String postcode, String destinationCity, String status) {
+		if (sender == null || streetAddress == null || postcode == null || destinationCity == null)
+			throw new IllegalArgumentException();
+		if(status != "Deliver" || status != "Pick up")
+			throw new IllegalArgumentException();
+
+		this.sender = sender;
+		this.addressee = addressee;
+		this.streetAddress = streetAddress;
+		this.postcode = postcode;
+		this.destinationCity = destinationCity;
+		this.id = NextID;
+		this.status = status;
+		NextID++;
+		DataAdapter.addPackage(this);
+	}
+}
