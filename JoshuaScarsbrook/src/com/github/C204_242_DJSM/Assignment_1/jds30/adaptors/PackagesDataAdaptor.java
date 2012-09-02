e1b4c32d940324b4bb9db016f5185e2b493457f0
Vbitz <jscarsbrook@gmail.com>
2012-09-03 11:23:17 +1200

    Started on the classes

---
+++ JoshuaScarsbrook/src/com/github/C204_242_DJSM/Assignment_1/jds30/adaptors/PackagesDataAdaptor.java
@@ -0,0 +1,8 @@
+package com.github.C204_242_DJSM.Assignment_1.jds30.adaptors;
+
+public class PackagesDataAdaptor {
+
+	public static PackagesDataAdaptor getDummyAdaptor() {
+		return new PackagesDataAdaptor();
+	}
+}
