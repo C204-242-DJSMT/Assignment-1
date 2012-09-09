d8ae3666ae8505151ec6d1792cb1348fd4494f04
Vbitz <jscarsbrook@gmail.com>
2012-09-10 00:33:31 +1200

    UI Done

--- JoshuaScarsbrook/src/com/github/C204_242_DJSM/Assignment_1/jds30/ui/ClientUI.java
+++ JoshuaScarsbrook/src/com/github/C204_242_DJSM/Assignment_1/jds30/ui/ClientUI.java
@@ -19,6 +19,14 @@ import javax.swing.JTextPane;
 
 import com.github.C204_242_DJSM.Assignment_1.jds30.adaptors.CurrentPackagesListModel;
 import com.github.C204_242_DJSM.Assignment_1.jds30.session.ClientSession;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.AbstractListModel;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class ClientUI extends JFrame {
 
@@ -39,86 +47,215 @@ public class ClientUI extends JFrame {
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		getContentPane().add(tabbedPane, BorderLayout.CENTER);
 		
-		JPanel panel = new JPanel();
-		tabbedPane.addTab("User Manager", null, panel, null);
-		panel.setLayout(null);
+		JPanel userManagerPane = new JPanel();
+		tabbedPane.addTab("User Manager", null, userManagerPane, null);
+		userManagerPane.setLayout(null);
 		
-		JList list = new JList();
-		list.setBounds(10, 11, 190, 391);
-		panel.add(list);
+		JList clientList = new JList();
+		clientList.setBounds(10, 11, 190, 391);
+		userManagerPane.add(clientList);
 		
-		JButton btnNewButton = new JButton("Create");
-		btnNewButton.setBounds(210, 379, 89, 23);
-		panel.add(btnNewButton);
+		JButton userCreateUpdateButton = new JButton("Create \\ Update");
+		userCreateUpdateButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				
+			}
+		});
+		userCreateUpdateButton.setBounds(210, 379, 116, 23);
+		userManagerPane.add(userCreateUpdateButton);
 		
-		JButton btnNewButton_1 = new JButton("Delete");
-		btnNewButton_1.setBounds(309, 379, 89, 23);
-		panel.add(btnNewButton_1);
+		JButton userDeleteButton = new JButton("Delete");
+		userDeleteButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+			}
+		});
+		userDeleteButton.setBounds(530, 379, 89, 23);
+		userManagerPane.add(userDeleteButton);
 		
-		JLabel lblNewLabel = new JLabel("Username");
-		lblNewLabel.setBounds(210, 12, 66, 14);
-		panel.add(lblNewLabel);
+		JLabel lblUserUsername = new JLabel("Username");
+		lblUserUsername.setBounds(210, 12, 66, 14);
+		userManagerPane.add(lblUserUsername);
 		
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(293, 11, 316, 20);
-		panel.add(textPane);
+		JTextField txtUserUsername = new JTextField();
+		txtUserUsername.setBounds(293, 11, 316, 20);
+		userManagerPane.add(txtUserUsername);
 		
 		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(210, 38, 66, 14);
-		panel.add(lblPassword);
+		lblPassword.setBounds(210, 190, 66, 14);
+		userManagerPane.add(lblPassword);
 		
-		JLabel lblNewLabel_1 = new JLabel("User Privlages");
-		lblNewLabel_1.setBounds(210, 63, 89, 14);
-		panel.add(lblNewLabel_1);
+		JLabel lblUserPrivlages = new JLabel("User Privlages");
+		lblUserPrivlages.setBounds(210, 218, 89, 14);
+		userManagerPane.add(lblUserPrivlages);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Full Manager", "Manager", "Employee"}));
-		comboBox.setBounds(293, 60, 316, 20);
-		panel.add(comboBox);
+		JComboBox<String> cboxUserPrivlages = new JComboBox<>();
+		cboxUserPrivlages.setModel(new DefaultComboBoxModel<String>(new String[] {"Full Manager", "Manager", "Employee"}));
+		cboxUserPrivlages.setBounds(293, 215, 316, 20);
+		userManagerPane.add(cboxUserPrivlages);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(293, 35, 316, 23);
-		panel.add(passwordField);
+		passUserPassword = new JPasswordField();
+		passUserPassword.setBounds(293, 187, 316, 23);
+		userManagerPane.add(passUserPassword);
 		
-		JPanel panel_2 = new JPanel();
-		tabbedPane.addTab("My Own Props", null, panel_2, null);
-		panel_2.setLayout(null);
+		JLabel lblUserAddresses = new JLabel("Addresses");
+		lblUserAddresses.setBounds(210, 37, 72, 14);
+		userManagerPane.add(lblUserAddresses);
 		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBounds(191, 137, 252, 118);
-		panel_2.add(panel_3);
-		panel_3.setLayout(null);
+		JComboBox cboxUserAddress = new JComboBox();
+		cboxUserAddress.setBounds(293, 34, 316, 20);
+		userManagerPane.add(cboxUserAddress);
 		
-		JLabel lblNewLabel_2 = new JLabel("Current Password");
-		lblNewLabel_2.setBounds(10, 11, 94, 14);
-		panel_3.add(lblNewLabel_2);
+		JLabel lblUserStreetAddress = new JLabel("Street Address");
+		lblUserStreetAddress.setBounds(293, 65, 81, 14);
+		userManagerPane.add(lblUserStreetAddress);
 		
-		JLabel lblNewPassword = new JLabel("New Password");
-		lblNewPassword.setBounds(10, 36, 94, 14);
-		panel_3.add(lblNewPassword);
+		JTextField txtUserStreetAddress = new JTextField();
+		txtUserStreetAddress.setBounds(384, 59, 225, 20);
+		userManagerPane.add(txtUserStreetAddress);
 		
-		JLabel lblConfirmPassword = new JLabel("Confirm Password");
-		lblConfirmPassword.setBounds(10, 61, 94, 14);
-		panel_3.add(lblConfirmPassword);
+		JLabel lblUserPostcode = new JLabel("Postcode");
+		lblUserPostcode.setBounds(293, 96, 81, 14);
+		userManagerPane.add(lblUserPostcode);
+		
+		JTextField txtUserPostcode = new JTextField();
+		txtUserPostcode.setBounds(384, 90, 225, 20);
+		userManagerPane.add(txtUserPostcode);
+		
+		JLabel lblUserCity = new JLabel("City");
+		lblUserCity.setBounds(293, 127, 81, 14);
+		userManagerPane.add(lblUserCity);
+		
+		JTextField txtUserCity = new JTextField();
+		txtUserCity.setBounds(384, 121, 225, 20);
+		userManagerPane.add(txtUserCity);
+		
+		JButton btnUserAddressDelete = new JButton("Delete");
+		btnUserAddressDelete.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+			}
+		});
+		btnUserAddressDelete.setBounds(520, 153, 89, 23);
+		userManagerPane.add(btnUserAddressDelete);
+		
+		JButton btnUserAddressUpdate = new JButton("Create \\ Update");
+		btnUserAddressUpdate.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+			}
+		});
+		btnUserAddressUpdate.setBounds(394, 153, 116, 23);
+		userManagerPane.add(btnUserAddressUpdate);
+		
+		JPanel passwordManagerPane = new JPanel();
+		tabbedPane.addTab("Personal Settings", null, passwordManagerPane, null);
+		passwordManagerPane.setLayout(null);
+		
+		JPanel passwordManagerPane2 = new JPanel();
+		passwordManagerPane2.setBounds(191, 137, 252, 118);
+		passwordManagerPane.add(passwordManagerPane2);
+		passwordManagerPane2.setLayout(null);
+		
+		JLabel lblPassCurrent = new JLabel("Current Password");
+		lblPassCurrent.setBounds(10, 11, 94, 14);
+		passwordManagerPane2.add(lblPassCurrent);
+		
+		JLabel lblPassNew = new JLabel("New Password");
+		lblPassNew.setBounds(10, 36, 94, 14);
+		passwordManagerPane2.add(lblPassNew);
+		
+		JLabel lblPassConfirm = new JLabel("Confirm Password");
+		lblPassConfirm.setBounds(10, 61, 94, 14);
+		passwordManagerPane2.add(lblPassConfirm);
 		
 		JButton btnChangePassword = new JButton("Change Password");
 		btnChangePassword.setBounds(123, 84, 119, 23);
-		panel_3.add(btnChangePassword);
+		passwordManagerPane2.add(btnChangePassword);
 		
-		passwordField_1 = new JPasswordField();
-		passwordField_1.setBounds(114, 8, 128, 20);
-		panel_3.add(passwordField_1);
+		passPassCurrent = new JPasswordField();
+		passPassCurrent.setBounds(114, 8, 128, 20);
+		passwordManagerPane2.add(passPassCurrent);
 		
-		passwordField_2 = new JPasswordField();
-		passwordField_2.setBounds(114, 33, 128, 20);
-		panel_3.add(passwordField_2);
+		passPassNew = new JPasswordField();
+		passPassNew.setBounds(114, 33, 128, 20);
+		passwordManagerPane2.add(passPassNew);
 		
-		passwordField_3 = new JPasswordField();
-		passwordField_3.setBounds(114, 58, 128, 20);
-		panel_3.add(passwordField_3);
+		passPassConfirm = new JPasswordField();
+		passPassConfirm.setBounds(114, 58, 128, 20);
+		passwordManagerPane2.add(passPassConfirm);
 		
-		JPanel panel_1 = new JPanel();
-		tabbedPane.addTab("Package Manager", null, panel_1, null);
+		JPanel packageManagerPane = new JPanel();
+		tabbedPane.addTab("Package Manager", null, packageManagerPane, null);
+		packageManagerPane.setLayout(null);
+		
+		JList packageList = new JList();
+		packageList.setBounds(10, 11, 190, 401);
+		packageManagerPane.add(packageList);
+		
+		JButton btnPackageSend = new JButton("Send");
+		btnPackageSend.setBounds(530, 389, 89, 23);
+		packageManagerPane.add(btnPackageSend);
+		
+		JLabel lblPackageReturnAddress = new JLabel("Return Address");
+		lblPackageReturnAddress.setBounds(210, 12, 96, 14);
+		packageManagerPane.add(lblPackageReturnAddress);
+		
+		JLabel lblPackageTargetAddress = new JLabel("Target Address");
+		lblPackageTargetAddress.setBounds(210, 37, 96, 14);
+		packageManagerPane.add(lblPackageTargetAddress);
+		
+		JLabel lblPackageStreetAddress = new JLabel("Street Address");
+		lblPackageStreetAddress.setBounds(316, 37, 78, 14);
+		packageManagerPane.add(lblPackageStreetAddress);
+		
+		txtPackageStreetAddress = new JTextField();
+		txtPackageStreetAddress.setBounds(404, 34, 215, 20);
+		packageManagerPane.add(txtPackageStreetAddress);
+		txtPackageStreetAddress.setColumns(10);
+		
+		JLabel lblPackagePostCode = new JLabel("Postcode");
+		lblPackagePostCode.setBounds(316, 65, 78, 14);
+		packageManagerPane.add(lblPackagePostCode);
+		
+		txtPackagePostCode = new JTextField();
+		txtPackagePostCode.setColumns(10);
+		txtPackagePostCode.setBounds(404, 62, 215, 20);
+		packageManagerPane.add(txtPackagePostCode);
+		
+		JLabel lblPackageCity = new JLabel("City");
+		lblPackageCity.setBounds(316, 93, 78, 14);
+		packageManagerPane.add(lblPackageCity);
+		
+		txtPackageCity = new JTextField();
+		txtPackageCity.setColumns(10);
+		txtPackageCity.setBounds(404, 90, 215, 20);
+		packageManagerPane.add(txtPackageCity);
+		
+		JComboBox<String> cboxPackageReturnAddress = new JComboBox<>();
+		cboxPackageReturnAddress.setBounds(316, 9, 303, 20);
+		packageManagerPane.add(cboxPackageReturnAddress);
+		
+		JLabel lblPackageStatus = new JLabel("Package Status");
+		lblPackageStatus.setBounds(210, 123, 96, 14);
+		packageManagerPane.add(lblPackageStatus);
+		
+		JList<String> packageStatusList = new JList<>();
+		packageStatusList.setModel(new AbstractListModel() {
+			String[] values = new String[] {};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		packageStatusList.setBounds(316, 122, 303, 254);
+		packageManagerPane.add(packageStatusList);
 		this.initControls();
 	}
 	
@@ -134,10 +271,14 @@ public class ClientUI extends JFrame {
 	
 	private CurrentPackagesListModel currentPackagesModel;
 	private JList<String> currentPackages;
-	private JPasswordField passwordField;
-	private JPasswordField passwordField_1;
-	private JPasswordField passwordField_2;
-	private JPasswordField passwordField_3;
+	private JPasswordField passUserPassword;
+	private JPasswordField passPassCurrent;
+	private JPasswordField passPassNew;
+	private JPasswordField passPassConfirm;
+	private JTextField txtPackageStreetAddress;
+	private JTextField txtPackagePostCode;
+	private JTextField txtPackageCity;
+	private final Action action = new SwingAction();
 	
 	// static methods
 
@@ -148,5 +289,13 @@ public class ClientUI extends JFrame {
 	 */
 	public static void main(String[] args) throws InterruptedException {
 		ClientLogin.main(args); // you should run login anyway
+	}
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "SwingAction");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
 	}
 }
